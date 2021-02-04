86189d72d9031cd3523c6b3bcc42683157f54e44
Nisim Hurst-Tarrab <langheran@gmail.com>
2021-02-04 12:39:46 -0600

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -2,6 +2,5 @@ virtualenv venv
 venv\Scripts\activate.bat
 pip install flask
 pip freeze > requirements.txt
-npm init
-npm install serverless@2.15.0 -U
+npm install serverless@2.15.0 -U -g
 npm install serverless-wsgi serverless-python-requirements --save
