57101af7b1c43c1291b29c8ada0ff643aa51f630
langheran <langheran@gmail.com>
2021-02-03 22:15:56 -0600

    changes

--- deploy_sls.bat
+++ deploy_sls.bat
@@ -1,3 +1,5 @@
+rmdir %USERPROFILE%\AppData\Local\UnitedIncome\serverless-python-requirements /s 
+@REM /q # for quiet 
 sls deploy -v --force --stage dev
 sls deploy function -f app -v --force --stage dev
 sls invoke -f app -l
