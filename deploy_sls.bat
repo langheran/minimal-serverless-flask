e7902b3c877a7c7746d5c4c92bc628afb34ed90d
langheran <langheran@gmail.com>
2021-02-08 19:13:17 -0600

    changes

--- deploy_sls.bat
+++ deploy_sls.bat
@@ -1,5 +1,9 @@
 rmdir %USERPROFILE%\AppData\Local\UnitedIncome\serverless-python-requirements /s 
 @REM /q # for quiet 
+
+venv\Scripts\activate.bat
+pip freeze > requirements.txt
+
 sls deploy -v --force --stage dev
 sls deploy function -f app -v --force --stage dev
 sls invoke -f app -l
--- setup.bat
+++ setup.bat
@@ -1,6 +1,6 @@
 virtualenv venv
 venv\Scripts\activate.bat
+@REM source venv/bin/activate
 pip install flask
-pip freeze > requirements.txt
 npm install serverless@2.15.0 -U -g
 npm install serverless-wsgi serverless-python-requirements --save
